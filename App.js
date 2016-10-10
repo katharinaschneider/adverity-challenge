b82a4586de2c0deb349dca3b2578cd8bb8f9c36a
Katharina <Katharina@defaults-Air.lan>
2016-10-10 15:53:07 +0200

    first impressions on fetching and displaying data

--- App.js
+++ App.js
@@ -1,15 +1,39 @@
 import React from 'react';
 import axios from 'axios';
-class App extends React.Component{
+class App extends React.Component {
   constructor(){
     super();
-    this.state = axios.get('http://mockbin.org/bin/3f1037be-88f3-4e34-a8ec-d602779bf2d6').then(data => {
-      console.log('csv lines', data.data.split('\n'))
-    })
+    this.state = {data: []};
   }
+
+  componentWillMount(){
+    axios.get('http://mockbin.org/bin/3f1037be-88f3-4e34-a8ec-d602779bf2d6').then(
+      resoponse => this.setState({data: resoponse.data.split('\n')}))
+  }
+
   render(){
-    return <div>this.state.data</div>
+    console.log(this.state.data);
+    return <div>{this.state.data[0]}</div>
+    // let rows = this.state.data.map( matrix => {
+    //   return <MatrixRow data={matrix} />
+    // })
+    // return (
+    //   <table>
+    //     <tbody>
+    //       {rows}
+    //     </tbody>
+    //   </table>
+    // );
   }
 }
+// const MatrixRow = (props) => {
+//   return <tr>
+//     <td>{props.data.campaign}</td>
+//     <td>{props.data.channel}</td>
+//     <td>{props.data.clicks}</td>
+//     <td>{props.data.impressions}</td>
+//   </tr>
+// }
+
 
 export default App
