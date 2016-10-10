73fe8808e99ca964dc97b98e9ee8b5471fc0ec5f
Katharina <Katharina@defaults-Air.lan>
2016-10-10 13:48:52 +0200

    testing fetching data with axis

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import axios from 'axios';
 class App extends React.Component{
+  constructor(){
+    super();
+    this.state = axios.get('http://mockbin.org/bin/3f1037be-88f3-4e34-a8ec-d602779bf2d6').then(data => {
+      console.log('csv lines', data.data.split('\n'))
+    })
+  }
   render(){
-    return <div>Hello</div>
+    return <div>this.state.data</div>
   }
 }
 
