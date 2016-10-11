5b02739f3c784fc298aecc20cc17c89d9759e36c
Katharina <Katharina@defaults-Air.lan>
2016-10-11 15:57:06 +0200

    first attempt of dropdown filled with data

--- App.js
+++ App.js
@@ -1,39 +1,71 @@
 import React from 'react';
 import axios from 'axios';
+import Select from 'react-select';
+
+// let Mixin = InnerComponent => class extends React.Component {
+//   constructor(){
+//     super();
+//     this.update = this.update.bind(this);
+//     this.state = {data: []}
+//   }
+//   update(){
+//     axios.get('http://mockbin.org/bin/3f1037be-88f3-4e34-a8ec-d602779bf2d6').then(
+//       resoponse => this.setState({data: resoponse.data.split('\n')}))
+//   }
+//   componentWillMount(){
+//     axios.get('http://mockbin.org/bin/3f1037be-88f3-4e34-a8ec-d602779bf2d6').then(
+//       resoponse => this.setState({data: resoponse.data.split('\n')}));
+//     console.log('will mount')
+//   }
+//   render(){
+//     return <InnerComponent
+//       update={this.update}
+//       {...this.state}
+//       {...this.props} />
+//   }
+//   componentDidMount(){
+//     console.log('mounted')
+//   }
+// }
+//
+// const SHOWDATA = (props) => {return props.data};
+//
+// let DataMixed = Mixin(SHOWDATA);
+
+
+const getOptions = (input) => {
+  let modifiedData = [];
+  let dropdownData = [{value: 'display', label: 'Display'}, {value: 'search', label: 'Search'}];
+
+  return axios.get('http://mockbin.org/bin/3f1037be-88f3-4e34-a8ec-d602779bf2d6').then((response) => {
+    let array = response.data.split('\n');
+    for (let i = 1; i < array.length; i++){
+      modifiedData[i] = array[i].split(',');
+      dropdownData.push({value: modifiedData[i][0], label: modifiedData[i][0]});
+    }
+    console.log('data', dropdownData);
+    return {options: dropdownData};
+  });
+}
+
+function logChange(val) {
+    console.log("Selected: " + val);
+}
+
+
+
 class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {data: []};
-  }
-
-  componentWillMount(){
-    axios.get('http://mockbin.org/bin/3f1037be-88f3-4e34-a8ec-d602779bf2d6').then(
-      resoponse => this.setState({data: resoponse.data.split('\n')}))
-  }
-
   render(){
-    console.log(this.state.data);
-    return <div>{this.state.data[0]}</div>
-    // let rows = this.state.data.map( matrix => {
-    //   return <MatrixRow data={matrix} />
-    // })
-    // return (
-    //   <table>
-    //     <tbody>
-    //       {rows}
-    //     </tbody>
-    //   </table>
-    // );
+    return(
+      <div id='app'>
+      <Select.Async
+          name="form-field-name"
+      
+          loadOptions={getOptions}
+      />
+      </div>
+    )
   }
 }
-// const MatrixRow = (props) => {
-//   return <tr>
-//     <td>{props.data.campaign}</td>
-//     <td>{props.data.channel}</td>
-//     <td>{props.data.clicks}</td>
-//     <td>{props.data.impressions}</td>
-//   </tr>
-// }
-
 
 export default App
