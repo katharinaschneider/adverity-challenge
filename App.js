41032be8c6ba0b4a54a8da25fb470849768b4336
Katharina <Katharina@defaults-Air.lan>
2016-10-12 13:08:57 +0200

    adding gitignore file

--- App.js
+++ App.js
@@ -2,6 +2,24 @@ import React from 'react';
 import axios from 'axios';
 import Select from 'react-select';
 import { uniq } from 'lodash/fp';
+import ReactDom from 'react-dom';
+
+var Clicks = React.createClass ({
+  render(){
+    console.log('props', this.props.renderForValue);
+    return (
+      <p>Clicks: {this.props.renderForValue.label}</p>
+    );
+  }
+});
+
+var Impressions = React.createClass ({
+  render(){
+    return (
+      <p>Impressions: </p>
+    );
+  }
+});
 
 
 const getOptions = (input) => {
@@ -14,33 +32,49 @@ const getOptions = (input) => {
       modifiedData[i] = array[i].split(',');
       dropdownData.push({'value': modifiedData[i][0], 'label': modifiedData[i][0]});
     }
-    console.log('hello');
     let uniqueData = uniq(dropdownData, 'value');
-    console.log('data', uniqueData);
+    //console.log('data', uniqueData);
     return {options: uniqueData};
   });
 }
 
 
 function logChange(val) {
-    console.log("Selected: " + val.value);
+    //console.log("Selected: " + val.value);
     return val;
 }
 
 
-class App extends React.Component {
+class ShowFilteredResults extends React.Component {
+  constructor(){
+    super();
+    this.state = {selected: [{'value': '', 'label': ''}]};
+    this.update = this.update.bind(this);
+  }
+
+  update(val) {
+    this.setState({
+      selected: [{value: val.value, label: val.label}]
+    });
+  }
+
   render(){
+    //console.log(this.state.selected);
     return(
       <div id='app'>
       <h1>Adverity</h1>
       <Select.Async
           name="form-field-name"
           loadOptions={getOptions}
-          onChange={logChange}
+          onChange={this.update}
+
       />
+      <Clicks renderForValue={this.state.selected} />
+      <Impressions renderForValue={this.state.selected} />
       </div>
+
     )
   }
 }
 
-export default App
+export default ShowFilteredResults
