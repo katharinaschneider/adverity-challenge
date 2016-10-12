13320a7008331357ab231855e02ffc694e17e570
Katharina <Katharina@defaults-Air.lan>
2016-10-12 11:10:54 +0200

    deleted commented sections

--- App.js
+++ App.js
@@ -3,36 +3,6 @@ import axios from 'axios';
 import Select from 'react-select';
 import { uniq } from 'lodash/fp';
 
-// let Mixin = InnerComponent => class extends React.Component {
-//   constructor(){
-//     super();
-//     this.update = this.update.bind(this);
-//     this.state = {data: []}
-//   }
-//   update(){
-//     axios.get('http://mockbin.org/bin/3f1037be-88f3-4e34-a8ec-d602779bf2d6').then(
-//       resoponse => this.setState({data: resoponse.data.split('\n')}))
-//   }
-//   componentWillMount(){
-//     axios.get('http://mockbin.org/bin/3f1037be-88f3-4e34-a8ec-d602779bf2d6').then(
-//       resoponse => this.setState({data: resoponse.data.split('\n')}));
-//     console.log('will mount')
-//   }
-//   render(){
-//     return <InnerComponent
-//       update={this.update}
-//       {...this.state}
-//       {...this.props} />
-//   }
-//   componentDidMount(){
-//     console.log('mounted')
-//   }
-// }
-//
-// const SHOWDATA = (props) => {return props.data};
-//
-// let DataMixed = Mixin(SHOWDATA);
-
 
 const getOptions = (input) => {
   let modifiedData = [];
@@ -62,6 +32,7 @@ class App extends React.Component {
   render(){
     return(
       <div id='app'>
+      <h1>Adverity</h1>
       <Select.Async
           name="form-field-name"
           loadOptions={getOptions}
